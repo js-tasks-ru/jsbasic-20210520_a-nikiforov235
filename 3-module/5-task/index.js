90f1bd285d7bb919edb959db42a15751ff7d9fca
Alexey Nikiforov <a.nikiforov235@gmail.com>
2021-06-09 22:00:13 +0300

    3 module 5 task

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,14 @@
 function getMinMax(str) {
   // ваш код...
+  const numbers = [];
+  str.split(' ').forEach(item =>
+    item.split(',').forEach(num => {
+    if (!isNaN(parseFloat(num))) {
+      numbers.push(parseFloat(num));
+    }
+  }));
+  return {
+    min: Math.min.apply(null, numbers),
+    max: Math.max.apply(null, numbers)
+  };
 }
