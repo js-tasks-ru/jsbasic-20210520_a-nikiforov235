51a485da1b0f79e6af359c562baa7a79ebdaca31
Alexey Nikiforov <a.nikiforov235@gmail.com>
2021-06-14 19:41:14 +0300

    4 module 1,2,3 tasks

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,8 @@
 function makeFriendsList(friends) {
   // ваш код...
+  let list = document.createElement('ul')
+  for (let elem of friends) {
+    list.innerHTML += `<li>${elem.firstName} ${elem.lastName}</li>`
+  }
+  return list
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,20 @@
 function highlight(table) {
   // ваш код...
-}
+  let rows = table.rows;
+  for (tr of rows) {
+  // обработка data-available
+  let status = tr.lastElementChild.getAttribute('data-available')
+  if (!status) {tr.setAttribute('hidden','true')}
+  if (status === 'true') {tr.classList.add('available')}
+  if (status === 'false') {tr.classList.add('unavailable')}
+  // обработка gender
+  let gender = tr.lastElementChild.previousElementSibling.innerHTML
+  if (gender === 'm') {tr.classList.add('male')}
+  if (gender === 'f') {tr.classList.add('female')}
+  //обработка age
+  let age = tr.firstElementChild.nextElementSibling.innerHTML
+  if (+age < 18) {tr.style.textDecoration = 'line-through'}
+  }
+};
+
+
